514b76c43e131a425b16d552dc458e1395960e42
Evolve <artevolve@yahoo.com>
2023-10-16 20:25:52 +0200

    ✅ typed_data

--- src/utils/signing/typed_data.rs
+++ src/utils/signing/typed_data.rs
@@ -6,12 +6,12 @@ use starknet::core::{
     utils::{cairo_short_string_to_felt, get_selector_from_name},
 };
 use std::fs;
+use std::io::Read;
 use std::path::Path;
 use std::{
     collections::{HashMap, HashSet},
     str::FromStr,
 };
-use std::io::Read;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct StarkNetDomain {
@@ -29,21 +29,15 @@ pub struct Parameter {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum ChainId {
-    String(String),
-    Number(u64),
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct DomainType {
-  name: String,
-  version: String,
-  #[serde(rename = "chainId")]
-  chain_id: ChainId,
+    name: String,
+    version: String,
+    #[serde(rename = "chainId")]
+    chain_id: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct TypedData {
+pub struct TypedData<T: Serialize> {
     #[serde(rename = "types")]
     types: HashMap<String, Vec<Parameter>>,
     #[serde(rename = "primaryType")]
@@ -51,10 +45,13 @@ pub struct TypedData {
     #[serde(rename = "domain")]
     domain: DomainType,
     #[serde(rename = "message")]
-    message: HashMap<String, String>,
+    message: T,
 }
 
-impl TypedData {
+impl<T> TypedData<T>
+where
+    T: Serialize,
+{
     /// Returns true if the type is a struct.
     ///
     /// # Arguments
@@ -252,7 +249,7 @@ impl TypedData {
     ///
     /// * The hash of the message.
     pub fn message_hash(&self, account_address: FieldElement) -> FieldElement {
-        let prefix = FieldElement::from_str("StarkNet Domain").unwrap();
+        let prefix = cairo_short_string_to_felt("StarkNet Message").unwrap();
 
         let json_str = serde_json::to_string(&self.domain).unwrap();
         let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
@@ -312,20 +309,71 @@ pub(crate) fn get_hex(value: &Value) -> Result<String, &'static str> {
 
 const TYPED_DATA_DIR: &str = "src/utils/signing/mock"; // Update this to your actual directory path.
 
-pub(crate) fn load_typed_data(file_name: &str) -> TypedData {
+pub(crate) fn load_typed_data<T>(file_name: &str) -> TypedData<T>
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
     let file_path = format!("{}/{}", TYPED_DATA_DIR, file_name);
     let path = Path::new(&file_path);
     let mut file = fs::File::open(&path).expect("Error opening the file");
     let mut buff = String::new();
     file.read_to_string(&mut buff).unwrap();
-    let typed_data: TypedData = serde_json::from_str(&buff).expect("Error parsing the JSON");
+    let typed_data: TypedData<T> = serde_json::from_str(&buff).expect("Error parsing the JSON");
     typed_data
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
     use super::*;
+    use rstest::rstest;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct WalletType {
+        name: String,
+        wallet: String,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MailType {
+        from: WalletType,
+        to: WalletType,
+        contents: String,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MailTypeFeltArray {
+        from: WalletType,
+        to: WalletType,
+        felts_len: u64,
+        felts: Vec<u64>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct ContentsType {
+        len: u64,
+        data: Vec<FieldElement>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MailTypeLongString {
+        from: WalletType,
+        to: WalletType,
+        contents: ContentsType,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Post {
+        title: String,
+        content: String,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MailTypeStructArray {
+        from: WalletType,
+        to: WalletType,
+        posts_len: u64,
+        posts: Vec<Post>,
+    }
 
     #[rstest]
     #[case(json!(123), "0x7b")]
@@ -347,23 +395,236 @@ mod tests {
         "Mail",
         "Mail(from:Person,to:Person,contents:felt)Person(name:felt,wallet:felt)"
     )]
+    fn test_encode_type_simple(
+        #[case] example: &str,
+        #[case] type_name: &str,
+        #[case] encoded_type: &str,
+    ) {
+        let typed_data: TypedData<MailType> = load_typed_data(example);
+        let res = typed_data.encode_type(type_name);
+        assert_eq!(res, encoded_type);
+    }
+
+    #[rstest]
     #[case(
         TD_FELT_ARR,
         "Mail",
         "Mail(from:Person,to:Person,felts_len:felt,felts:felt*)Person(name:felt,wallet:felt)"
     )]
-    #[case(TD_STRING, "Mail", "Mail(from:Person,to:Person,contents:String)Person(name:felt,wallet:felt)String(len:felt,data:felt*)")]
-    #[case(TD_STRUCT_ARR, "Mail", "Mail(from:Person,to:Person,posts_len:felt,posts:Post*)Person(name:felt,wallet:felt)Post(title:felt,content:felt)")]
-    fn test_encode_type(
+    fn test_encode_type_felt_array(
         #[case] example: &str,
         #[case] type_name: &str,
         #[case] encoded_type: &str,
     ) {
-        println!("example: {:?}", example);
-        println!("type_name: {:?}", type_name);
-        println!("encoded_type: {:?}", encoded_type);
-        let typed_data = load_typed_data(example);
+        let typed_data: TypedData<MailTypeFeltArray> = load_typed_data(example);
         let res = typed_data.encode_type(type_name);
         assert_eq!(res, encoded_type);
+    }
+
+    #[rstest]
+    #[case(TD_STRING, "Mail", "Mail(from:Person,to:Person,contents:String)Person(name:felt,wallet:felt)String(len:felt,data:felt*)")]
+    fn test_encode_type_long_string(
+        #[case] example: &str,
+        #[case] type_name: &str,
+        #[case] encoded_type: &str,
+    ) {
+        let typed_data: TypedData<MailTypeLongString> = load_typed_data(example);
+        let res = typed_data.encode_type(type_name);
+        assert_eq!(res, encoded_type);
+    }
+
+    #[rstest]
+    #[case(TD_STRUCT_ARR, "Mail", "Mail(from:Person,to:Person,posts_len:felt,posts:Post*)Person(name:felt,wallet:felt)Post(title:felt,content:felt)")]
+    fn test_encode_type_struct_array(
+        #[case] example: &str,
+        #[case] type_name: &str,
+        #[case] encoded_type: &str,
+    ) {
+        let typed_data: TypedData<MailTypeStructArray> = load_typed_data(example);
+        let res = typed_data.encode_type(type_name);
+        assert_eq!(res, encoded_type);
+    }
+
+    #[rstest]
+    #[case(
+        TD,
+        "StarkNetDomain",
+        "0x1bfc207425a47a5dfa1a50a4f5241203f50624ca5fdf5e18755765416b8e288"
+    )]
+    #[case(
+        TD,
+        "Person",
+        "0x2896dbe4b96a67110f454c01e5336edc5bbc3635537efd690f122f4809cc855"
+    )]
+    #[case(
+        TD,
+        "Mail",
+        "0x13d89452df9512bf750f539ba3001b945576243288137ddb6c788457d4b2f79"
+    )]
+    #[case(
+        TD_STRING,
+        "String",
+        "0x1933fe9de7e181d64298eecb44fc43b4cec344faa26968646761b7278df4ae2"
+    )]
+    #[case(
+        TD_STRING,
+        "Mail",
+        "0x1ac6f84a5d41cee97febb378ddabbe1390d4e8036df8f89dee194e613411b09"
+    )]
+    #[case(
+        TD_FELT_ARR,
+        "Mail",
+        "0x5b03497592c0d1fe2f3667b63099761714a895c7df96ec90a85d17bfc7a7a0"
+    )]
+    #[case(
+        TD_STRUCT_ARR,
+        "Post",
+        "0x1d71e69bf476486b43cdcfaf5a85c00bb2d954c042b281040e513080388356d"
+    )]
+    #[case(
+        TD_STRUCT_ARR,
+        "Mail",
+        "0x873b878e35e258fc99e3085d5aaad3a81a0c821f189c08b30def2cde55ff27"
+    )]
+    fn test_type_hash(
+        #[case] example: &str,
+        #[case] type_name: &str,
+        #[case] expected_type_hash: &str,
+    ) {
+        match example {
+            TD => {
+                let typed_data: TypedData<MailType> = load_typed_data(example);
+                let result = typed_data.type_hash(type_name);
+                assert_eq!(format!("{:#x}", result), expected_type_hash);
+            }
+            TD_STRING => {
+                let typed_data: TypedData<MailTypeLongString> = load_typed_data(example);
+                let result = typed_data.type_hash(type_name);
+                assert_eq!(format!("{:#x}", result), expected_type_hash);
+            }
+            TD_FELT_ARR => {
+                let typed_data: TypedData<MailTypeFeltArray> = load_typed_data(example);
+                let result = typed_data.type_hash(type_name);
+                assert_eq!(format!("{:#x}", result), expected_type_hash);
+            }
+            TD_STRUCT_ARR => {
+                let typed_data: TypedData<MailTypeStructArray> = load_typed_data(example);
+                let result = typed_data.type_hash(type_name);
+                assert_eq!(format!("{:#x}", result), expected_type_hash);
+            }
+            _ => panic!("Unknown example type"),
+        }
+    }
+
+    #[test]
+    fn test_struct_hash_starknet_domain() {
+        let typed_data: TypedData<MailType> = load_typed_data(TD);
+        let json_str = serde_json::to_string(&typed_data.domain).unwrap();
+        let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
+        let result = typed_data.struct_hash("StarkNetDomain", &json_map);
+        assert_eq!(
+            format!("{:#x}", result),
+            "0x54833b121883a3e3aebff48ec08a962f5742e5f7b973469c1f8f4f55d470b07"
+        );
+    }
+
+    #[test]
+    fn test_struct_hash_mail_message() {
+        let typed_data: TypedData<MailType> = load_typed_data(TD);
+        let json_str = serde_json::to_string(&typed_data.message).unwrap();
+        let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
+        let result = typed_data.struct_hash("Mail", &json_map);
+        assert_eq!(
+            format!("{:#x}", result),
+            "0x4758f1ed5e7503120c228cbcaba626f61514559e9ef5ed653b0b885e0f38aec"
+        );
+    }
+
+    #[test]
+    fn test_struct_hash_mail_long_string() {
+        let typed_data: TypedData<MailTypeLongString> = load_typed_data(TD_STRING);
+        let json_str = serde_json::to_string(&typed_data.message).unwrap();
+        let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
+        let result = typed_data.struct_hash("Mail", &json_map);
+        assert_eq!(
+            format!("{:#x}", result),
+            "0x1d16b9b96f7cb7a55950b26cc8e01daa465f78938c47a09d5a066ca58f9936f"
+        );
+    }
+
+    #[test]
+    fn test_struct_hash_mail_felt_array() {
+        let typed_data: TypedData<MailTypeFeltArray> = load_typed_data(TD_FELT_ARR);
+        let json_str = serde_json::to_string(&typed_data.message).unwrap();
+        let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
+        let result = typed_data.struct_hash("Mail", &json_map);
+        assert_eq!(
+            format!("{:#x}", result),
+            "0x26186b02dddb59bf12114f771971b818f48fad83c373534abebaaa39b63a7ce"
+        );
+    }
+
+    // TODO: Fix this test `called `Result::unwrap()` on an `Err` value: "Unsupported value type for get_hex"`
+    // #[test]
+    // fn test_struct_hash_mail_struct_array() {
+    //     let typed_data: TypedData<MailTypeStructArray> = load_typed_data(TD_STRUCT_ARR);
+    //     let json_str = serde_json::to_string(&typed_data.message).unwrap();
+    //     let json_map: Map<String, Value> = serde_json::from_str(&json_str).unwrap();
+    //     let result = typed_data.struct_hash("Mail", &json_map);
+    //     assert_eq!(
+    //         format!("{:#x}", result),
+    //         "0x5650ec45a42c4776a182159b9d33118a46860a6e6639bb8166ff71f3c41eaef"
+    //     );
+    // }
+
+    #[rstest]
+    #[case(
+        TD,
+        "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+        "0x6fcff244f63e38b9d88b9e3378d44757710d1b244282b435cb472053c8d78d0"
+    )]
+    #[case(
+        TD_STRING,
+        "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+        "0x691b977ee0ee645647336f01d724274731f544ad0d626b078033d2541ee641d"
+    )]
+    #[case(
+        TD_FELT_ARR,
+        "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+        "0x30ab43ef724b08c3b0a9bbe425e47c6173470be75d1d4c55fd5bf9309896bce"
+    )]
+    // #[case(
+    //     TD_STRUCT_ARR,
+    //     "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+    //     "0x5914ed2764eca2e6a41eb037feefd3d2e33d9af6225a9e7fe31ac943ff712c"
+    // )]
+    fn test_message_hash(
+        #[case] example: &str,
+        #[case] account_address: &str,
+        #[case] msg_hash: &str,
+    ) {
+        let account_address = FieldElement::from_hex_be(account_address).unwrap();
+
+        let result = match example {
+            TD => {
+                let typed_data: TypedData<MailType> = load_typed_data(example);
+                typed_data.message_hash(account_address)
+            }
+            TD_STRING => {
+                let typed_data: TypedData<MailTypeLongString> = load_typed_data(example);
+                typed_data.message_hash(account_address)
+            }
+            TD_FELT_ARR => {
+                let typed_data: TypedData<MailTypeFeltArray> = load_typed_data(example);
+                typed_data.message_hash(account_address)
+            }
+            // TD_STRUCT_ARR => {
+            //     let typed_data: TypedData<MailTypeStructArray> = load_typed_data(example);
+            //     typed_data.message_hash(account_address)
+            // }
+            _ => panic!("Unsupported example type"),
+        };
+
+        assert_eq!(format!("{:#x}", result), msg_hash);
     }
 }
