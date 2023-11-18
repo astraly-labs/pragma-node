32d4e06e6b8417748655f0a80a777d7855b0fd33
Evolve <artevolve@yahoo.com>
2023-11-18 16:17:35 +0300

    ⚡️ pair_id db index

---
+++ migrations/2023-11-18-131303_add_entries_index/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+CREATE INDEX idx_entries_pair_id ON entries(pair_id);
--- src/infra/db/schema.rs
+++ src/infra/db/schema.rs
@@ -33,4 +33,8 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(currencies, entries, publishers,);
+diesel::allow_tables_to_appear_in_same_query!(
+    currencies,
+    entries,
+    publishers,
+);
