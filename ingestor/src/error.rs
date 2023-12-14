46a88d4f167aeeee68f78274f307d9caef12e3de
graniet <graniet75@gmail.com>
2023-12-14 22:45:07 +0100

    refactoring

--- ingestor/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum ErrorKind {
-    #[error("read config error: {0}")]
-    ReadConfig(#[from] std::io::Error),
-    #[error("load config error: {0}")]
-    LoadConfig(#[from] toml::de::Error),
-}
