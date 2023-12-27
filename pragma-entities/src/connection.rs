93d00e63a584a92118cada432180537c430ca5c0
0xevolve <Artevolve@yahoo.com>
2023-12-27 13:36:47 +0100

    fix: clippy

--- pragma-entities/src/connection.rs
+++ pragma-entities/src/connection.rs
@@ -6,10 +6,10 @@ const ENV_DATABASE_MAX_CONN: &str = "DATABASE_MAX_CONN";
 
 pub fn init_pool(app_name: &str) -> Result<Pool, ErrorKind> {
     let database_url = std::env::var(ENV_DATABASE_URL)
-        .map_err(|_| ErrorKind::VariableDatabase(format!("{}", ENV_DATABASE_URL)))?;
+        .map_err(|_| ErrorKind::VariableDatabase(ENV_DATABASE_URL.to_string()))?;
 
     let database_max_conn = std::env::var(ENV_DATABASE_MAX_CONN)
-        .map_err(|_| ErrorKind::VariableDatabase(format!("{}", ENV_DATABASE_MAX_CONN)))?
+        .map_err(|_| ErrorKind::VariableDatabase(ENV_DATABASE_MAX_CONN.to_string()))?
         .parse::<u32>()
         .map_err(|_| {
             ErrorKind::GenericInitDatabase(format!("cannot parse {}", ENV_DATABASE_MAX_CONN))
