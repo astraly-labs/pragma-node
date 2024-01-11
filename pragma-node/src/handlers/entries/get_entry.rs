0674de9eeb2d49cc82392211b13422b37f515b3b
0xevolve <Artevolve@yahoo.com>
2024-01-11 16:06:01 +0100

    feat: adapt route to use new views

--- pragma-node/src/handlers/entries/get_entry.rs
+++ pragma-node/src/handlers/entries/get_entry.rs
@@ -1,6 +1,6 @@
 use axum::extract::State;
 use axum::Json;
-use bigdecimal::num_bigint::{BigInt, ToBigInt};
+use bigdecimal::num_bigint::ToBigInt;
 
 use crate::handlers::entries::GetEntryResponse;
 use crate::infra::repositories::entry_repository::{self, MedianEntry};
@@ -8,7 +8,7 @@ use crate::utils::PathExtractor;
 use crate::AppState;
 use pragma_entities::{error::InfraError, EntryError};
 
-use super::utils::{compute_median_price_and_time, currency_pair_to_pair_id};
+use super::utils::currency_pair_to_pair_id;
 
 #[utoipa::path(
         get,
@@ -41,18 +41,13 @@ pub async fn get_entry(
     }
 
     // Get entries from database with given pair id (only the latest one grouped by publisher)
-    let mut entries = entry_repository::get_median_entries(&state.pool, pair_id.clone())
+    let entry = entry_repository::get_median_price(&state.pool, pair_id.clone())
         .await
         .map_err(|db_error| match db_error {
             InfraError::InternalServerError => EntryError::InternalServerError,
             InfraError::NotFound => EntryError::NotFound(pair_id.clone()),
             InfraError::InvalidTimeStamp => EntryError::InvalidTimestamp,
         })?;
-
-    // Error if no entries found
-    if entries.is_empty() {
-        return Err(EntryError::UnknownPairId(pair_id));
-    }
 
     let decimals = entry_repository::get_decimals(&state.pool, &pair_id)
         .await
@@ -63,28 +58,25 @@ pub async fn get_entry(
         })?;
 
     Ok(Json(adapt_entry_to_entry_response(
-        pair_id,
-        &mut entries,
-        decimals,
+        pair_id, &entry, decimals,
     )))
 }
 
 fn adapt_entry_to_entry_response(
     pair_id: String,
-    entries: &mut Vec<MedianEntry>,
+    entry: &MedianEntry,
     decimals: u32,
 ) -> GetEntryResponse {
-    let (price, timestamp) = compute_median_price_and_time(entries).unwrap_or_default();
-
     GetEntryResponse {
         pair_id,
-        timestamp: timestamp.timestamp_millis() as u64,
-        num_sources_aggregated: entries.len(),
+        timestamp: entry.time.timestamp_millis() as u64,
+        num_sources_aggregated: entry.num_sources as usize,
         price: format!(
             "0x{}",
-            price
+            entry
+                .median_price
                 .to_bigint()
-                .unwrap_or(BigInt::default())
+                .unwrap_or_default()
                 .to_str_radix(16)
         ),
         decimals,
