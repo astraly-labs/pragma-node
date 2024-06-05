ed8d0d28776deb004bf4abe8620cc4b4b62536b0
akhercha <akherchache@pm.me>
2024-06-05 11:15:08 +0200

    feat(starkex_handler): Utils starkex functions

--- pragma-node/src/utils/starkex/mod.rs
+++ pragma-node/src/utils/starkex/mod.rs
@@ -1,46 +1,60 @@
-use bigdecimal::BigDecimal;
+use bigdecimal::{BigDecimal, ToPrimitive};
 
 use starknet::{
     core::{
         crypto::{pedersen_hash, EcdsaSignError, Signature},
         types::FieldElement,
+        utils::cairo_short_string_to_felt,
     },
     signers::SigningKey,
 };
 
+fn build_first_number(oracle_name: &str, pair_id: &str) -> FieldElement {
+    let oracle_name = cairo_short_string_to_felt(oracle_name).unwrap();
+    let oracle_as_hex = format!("{:x}", oracle_name);
+    let pair_id = cairo_short_string_to_felt(pair_id).unwrap();
+    let pair_id: u128 = pair_id.try_into().unwrap();
+    let pair_as_hex = format!("{:0<width$x}", pair_id, width = 32);
+    let v = format!("{}{}", pair_as_hex, oracle_as_hex);
+    FieldElement::from_hex_be(&v).unwrap()
+}
+
+fn build_second_number(timestamp: u64, price: BigDecimal) -> FieldElement {
+    // TODO(akhercha): round?
+    let price = price.round(2);
+    // TODO(akhercha): 18 all the time ? Or can be different depending on pairs?
+    let price = price * BigDecimal::from(10_u128.pow(18));
+    let price = price.to_u128().unwrap();
+    let price_as_hex = format!("{:x}", price);
+    let timestamp: u128 = timestamp.into();
+    let timestamp_as_hex = format!("{:x}", timestamp);
+    println!("{}", timestamp_as_hex);
+    let v = format!("{}{}", price_as_hex, timestamp_as_hex);
+    FieldElement::from_hex_be(&v).unwrap()
+}
+
 #[allow(dead_code)]
 pub fn get_price_message(
-    _oracle_name: &str,
-    _pair_id: &str,
-    _timestamp: u64,
-    _price: BigDecimal,
+    // TODO(akhercha): oracle name should be a constant "Pragma"
+    oracle_name: &str,
+    pair_id: &str,
+    timestamp: u64,
+    price: BigDecimal,
 ) -> FieldElement {
     // TODO(akhercha): Build number A & B from the input data.
     // 1. Build number A from oracle_name & pair_id
-    let a = FieldElement::from_hex_be("425443555344000000000000000000004d616b6572").unwrap();
+    let first_number = build_first_number(oracle_name, pair_id);
     // 2. Build number B from price & timestamp
-    let b = FieldElement::from_hex_be("27015cfcb02308200005e0be100").unwrap();
-    pedersen_hash(&a, &b)
+    let second_number = build_second_number(timestamp, price);
+    pedersen_hash(&first_number, &second_number)
 }
 
 /// Sign the hashed_data using the private_key.
-/// See get_price_message for hashed_data context.
-///
-/// TODO: Assumes we already have a private key registered -somewhere-
-/// and passed here as a FieldElement.
-///
-/// E.g:
-/// key = 178047D3869489C055D7EA54C014FFB834A069C9595186ABE04EA4D1223A03F
-/// data = 3e4113feb6c403cb0c954e5c09d239bf88fedb075220270f44173ac3cd41858
-/// gives
-/// r: 0x6a7a118a6fa508c4f0eb77ea0efbc8d48a64d4a570d93f5c61cd886877cb920
-/// s: 0x6de9006a7bbf610d583d514951c98d15b1a0f6c78846986491d2c8ca049fd55
 #[allow(dead_code)]
 pub fn sign(
-    private_key: FieldElement,
+    signing_key: SigningKey,
     hashed_data: FieldElement,
 ) -> Result<Signature, EcdsaSignError> {
-    let signing_key = SigningKey::from_secret_scalar(private_key);
     signing_key.sign(&hashed_data)
 }
 
@@ -75,13 +89,14 @@ mod tests {
             "178047D3869489C055D7EA54C014FFB834A069C9595186ABE04EA4D1223A03F",
         )
         .unwrap();
+        let signing_key = SigningKey::from_secret_scalar(private_key);
         let hashed_data = FieldElement::from_hex_be(
             "3e4113feb6c403cb0c954e5c09d239bf88fedb075220270f44173ac3cd41858",
         )
         .unwrap();
 
         // 2. Action
-        let signature = sign(private_key, hashed_data).unwrap();
+        let signature = sign(signing_key, hashed_data).unwrap();
 
         // 3. Check
         let expected_r = FieldElement::from_hex_be(
