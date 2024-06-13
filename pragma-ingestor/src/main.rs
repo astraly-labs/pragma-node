2bd4aa64fe89f80d8f2f91ada1f15314080b4f6a
akhercha <akherchache@pm.me>
2024-06-13 13:50:51 +0200

    feat(starkex_handler): ingestor flow

--- pragma-ingestor/src/main.rs
+++ pragma-ingestor/src/main.rs
@@ -43,7 +43,7 @@ async fn process_payload(pool: &Pool, payload: Vec<u8>) -> Result<(), Box<dyn st
         match serde_json::from_slice::<Vec<NewEntry>>(&payload) {
             Ok(entries) => {
                 info!("[SPOT] total of '{}' new entries available.", entries.len());
-                if let Err(e) = insert_entries(pool, entries).await {
+                if let Err(e) = insert_spot_entries(pool, entries).await {
                     error!("error while inserting entries : {:?}", e);
                 }
             }
@@ -53,13 +53,25 @@ async fn process_payload(pool: &Pool, payload: Vec<u8>) -> Result<(), Box<dyn st
         }
     } else {
         match serde_json::from_slice::<Vec<NewFutureEntry>>(&payload) {
-            Ok(future_entries) => {
-                info!(
-                    "[FUTURE/PERP] total of '{}' new entries available.",
-                    future_entries.len()
-                );
-                if let Err(e) = insert_future_entries(pool, future_entries).await {
-                    error!("error while inserting future entries : {:?}", e);
+            Ok(mut future_entries) => {
+                let new_perp_entries = future_entries
+                    .iter()
+                    .filter(|entry| entry.expiration_timestamp.and_utc().timestamp() == 0)
+                    .map(|entry| NewPerpEntry::from(entry.clone()))
+                    .collect::<Vec<NewPerpEntry>>();
+                if !new_perp_entries.is_empty() {
+                    info!("[PERP] {} new entries available", new_perp_entries.len());
+                    if let Err(e) = insert_perp_entries(pool, new_perp_entries).await {
+                        error!("error while inserting perp entries : {:?}", e);
+                    }
+                }
+
+                future_entries.retain(|entry| entry.expiration_timestamp.and_utc().timestamp() > 0);
+                if !future_entries.is_empty() {
+                    info!("[FUTURE] {} new entries available", future_entries.len());
+                    if let Err(e) = insert_future_entries(pool, future_entries).await {
+                        error!("error while inserting future entries : {:?}", e);
+                    }
                 }
             }
             Err(e) => {
@@ -71,7 +83,10 @@ async fn process_payload(pool: &Pool, payload: Vec<u8>) -> Result<(), Box<dyn st
 }
 
 // TODO: move this to a service
-pub async fn insert_entries(pool: &Pool, new_entries: Vec<NewEntry>) -> Result<(), InfraError> {
+pub async fn insert_spot_entries(
+    pool: &Pool,
+    new_entries: Vec<NewEntry>,
+) -> Result<(), InfraError> {
     let conn = pool.get().await.map_err(adapt_infra_error)?;
     let entries = conn
         .interact(move |conn| Entry::create_many(conn, new_entries))
@@ -98,56 +113,36 @@ pub async fn insert_future_entries(
     pool: &Pool,
     new_entries: Vec<NewFutureEntry>,
 ) -> Result<(), InfraError> {
-    // TODO(akhercha): creating a connection pool for each request is not efficient
-    // filter out in two groups: perp & future
-    let new_perp_entries = new_entries
-        .iter()
-        .filter(|entry| entry.expiration_timestamp.and_utc().timestamp() == 0)
-        .map(|entry| NewPerpEntry::from(entry.clone()))
-        .collect::<Vec<NewPerpEntry>>();
-
     let conn = pool.get().await.map_err(adapt_infra_error)?;
-    if !new_perp_entries.is_empty() {
+    let entries = conn
+        .interact(move |conn| FutureEntry::create_many(conn, new_entries))
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    for entry in &entries {
         info!(
-            "[PERP] total of '{}' new entries available.",
-            new_perp_entries.len()
+            "new future entry created {} - {}({}) - {}",
+            entry.publisher, entry.pair_id, entry.price, entry.source
         );
-        let entries = conn
-            .interact(move |conn| PerpEntry::create_many(conn, new_perp_entries))
-            .await
-            .map_err(adapt_infra_error)?
-            .map_err(adapt_infra_error)?;
-        for entry in &entries {
-            info!(
-                "new perp entry created {} - {}({}) - {}",
-                entry.publisher, entry.pair_id, entry.price, entry.source
-            );
-        }
     }
+    Ok(())
+}
 
-    let new_future_entries = new_entries
-        .iter()
-        .filter(|entry| entry.expiration_timestamp.and_utc().timestamp() > 0)
-        .cloned()
-        .collect::<Vec<NewFutureEntry>>();
-
-    if !new_future_entries.is_empty() {
+pub async fn insert_perp_entries(
+    pool: &Pool,
+    new_entries: Vec<NewPerpEntry>,
+) -> Result<(), InfraError> {
+    let conn = pool.get().await.map_err(adapt_infra_error)?;
+    let entries = conn
+        .interact(move |conn| PerpEntry::create_many(conn, new_entries))
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    for entry in &entries {
         info!(
-            "[FUTURE] total of '{}' new entries available.",
-            new_future_entries.len()
+            "new perp entry created {} - {}({}) - {}",
+            entry.publisher, entry.pair_id, entry.price, entry.source
         );
-        let entries = conn
-            .interact(move |conn| FutureEntry::create_many(conn, new_future_entries))
-            .await
-            .map_err(adapt_infra_error)?
-            .map_err(adapt_infra_error)?;
-        for entry in &entries {
-            info!(
-                "new future entry created {} - {}({}) - {}",
-                entry.publisher, entry.pair_id, entry.price, entry.source
-            );
-        }
     }
-
     Ok(())
 }
