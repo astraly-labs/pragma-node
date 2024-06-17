b9fdf58ae909a1dfb9f70a799669efd45e0376b1
akhercha <akherchache@pm.me>
2024-06-17 21:50:39 +0200

    feat(better_ws): Iterating...

--- pragma-node/src/types/ws.rs
+++ pragma-node/src/types/ws.rs
@@ -1,3 +1,4 @@
+use serde::Deserialize;
 use std::fmt::Debug;
 use std::net::IpAddr;
 use std::sync::Arc;
@@ -13,8 +14,6 @@ use tokio::sync::watch;
 use tokio::time::{interval, Interval};
 use uuid::Uuid;
 
-pub type SubscriberId = Uuid;
-
 #[derive(Debug, Error)]
 pub enum WebSocketError {
     #[error("could not create a channel with the client")]
@@ -24,7 +23,7 @@ pub enum WebSocketError {
 /// Subscriber is an actor that handles a single websocket connection.
 /// It listens to the store for updates and sends them to the client.
 pub struct Subscriber<WsState> {
-    pub id: SubscriberId,
+    pub id: Uuid,
     pub ip_address: IpAddr,
     pub closed: bool,
     pub _app_state: Arc<AppState>,
@@ -37,35 +36,23 @@ pub struct Subscriber<WsState> {
 }
 
 pub trait ChannelHandler<WsState, R, A> {
-    /// TODO.
-    async fn decode_client_msg(
-        &mut self,
-        subscriber: &mut Subscriber<WsState>,
-        msg: Message,
-    ) -> Option<R>;
-
-    /// TODO
+    /// Called after a message is received from the client.
+    /// The handler should process the message and update the state.
     async fn handle_client_msg(
         &mut self,
         subscriber: &mut Subscriber<WsState>,
         message: R,
     ) -> Result<(), WebSocketError>;
 
-    /// TODO.
-    async fn decode_server_msg(
-        &mut self,
-        subscriber: &mut Subscriber<WsState>,
-        msg: Message,
-    ) -> Option<A>;
-
-    /// TODO.
+    /// Called after a message is received from the server.
+    /// The handler should process the message and update the state.
     async fn handle_server_msg(
         &mut self,
         subscriber: &mut Subscriber<WsState>,
         message: A,
     ) -> Result<(), WebSocketError>;
 
-    /// TODO.
+    /// Called at a regular interval to update the client with the latest state.
     async fn periodic_interval(
         &mut self,
         subscriber: &mut Subscriber<WsState>,
@@ -116,6 +103,8 @@ where
     pub async fn listen<H, R, A>(&mut self, mut handler: H) -> Result<(), WebSocketError>
     where
         H: ChannelHandler<WsState, R, A>,
+        R: for<'a> Deserialize<'a>,
+        A: for<'a> Deserialize<'a>,
     {
         loop {
             tokio::select! {
@@ -124,7 +113,7 @@ where
                     match maybe_client_msg {
                         Some(Ok(client_msg)) => {
                         tracing::info!("👤 [CLIENT MESSAGE]");
-                            let client_msg = handler.decode_client_msg(self, client_msg).await;
+                            let client_msg = self.decode_msg::<R>(client_msg).await;
                             if let Some(client_msg) = client_msg {
                                 handler.handle_client_msg(self, client_msg).await?;
                             }
@@ -145,7 +134,7 @@ where
                 maybe_server_msg = self.notify_receiver.recv() => {
                     if let Some(server_msg) = maybe_server_msg {
                         tracing::info!("🥡 [SERVER MESSAGE]");
-                        let server_msg = handler.decode_server_msg(self, server_msg).await;
+                        let server_msg = self.decode_msg::<A>(server_msg).await;
                         if let Some(sever_msg) = server_msg {
                             handler.handle_server_msg(self, sever_msg).await?;
                         }
@@ -155,10 +144,51 @@ where
                 _ = self.exit.1.changed() => {
                     if *self.exit.1.borrow() {
                         tracing::info!("⛔ [CLOSING SIGNAL]");
+                        self.closed = true;
                         return Ok(());
                     }
                 },
             }
         }
+    }
+
+    pub async fn decode_msg<T: for<'a> Deserialize<'a>>(&mut self, msg: Message) -> Option<T> {
+        match msg {
+            Message::Close(_) => {
+                tracing::info!("📨 [CLOSE]");
+                match self.exit.0.send(true) {
+                    Ok(_) => {
+                        self.closed = true;
+                        return None;
+                    }
+                    Err(_) => {
+                        tracing::error!("😱 Could not send close signal");
+                        return None;
+                    }
+                }
+            }
+            Message::Text(text) => {
+                tracing::info!("📨 [TEXT]");
+                let msg = serde_json::from_str::<T>(&text);
+                if let Ok(msg) = msg {
+                    return Some(msg);
+                } else {
+                    tracing::error!("😱 Could not decode message from client");
+                    return None;
+                }
+            }
+            Message::Binary(payload) => {
+                tracing::info!("📨 [BINARY]");
+                let maybe_msg = serde_json::from_slice::<T>(&payload);
+                if let Ok(msg) = maybe_msg {
+                    return Some(msg);
+                } else {
+                    tracing::error!("😱 Could not decode message from server");
+                    return None;
+                }
+            }
+            _ => {}
+        }
+        None
     }
 }
