1c55849fd47c66f4ed1922b3d66c81907242e9d3
adel <akherchache@pm.me>
2024-07-03 14:24:10 +0200

    fix: OHLC computation method (#75)
    * feat: new ohlc
    
    * feat(fix-ohlc):
    
    ---------
    
    Co-authored-by: 0xevolve <Artevolve@yahoo.com>

---
+++ pragma-entities/migrations/2024-07-02-232003_add_median_aggregates/down.sql
@@ -0,0 +1,2 @@
+-- This file should undo anything in `up.sql`
+DROP MATERIALIZED VIEW IF EXISTS price_10_s_agg;
--- pragma-node/src/infra/repositories/entry_repository.rs
+++ pragma-node/src/infra/repositories/entry_repository.rs
@@ -530,88 +530,27 @@ pub async fn get_ohlc(
 ) -> Result<Vec<OHLCEntry>, InfraError> {
     let conn = pool.get().await.map_err(adapt_infra_error)?;
 
-    let raw_sql = match interval {
-        Interval::OneMinute => {
-            r#"
+    let raw_sql = format!(
+        r#"
         -- query the materialized realtime view
         SELECT
-            bucket AS time,
+            ohlc_bucket AS time,
             open,
             high,
             low,
             close
         FROM
-            one_minute_candle
+            new_{}_candle
         WHERE
             pair_id = $1
             AND
-            bucket <= $2
+            ohlc_bucket <= $2
         ORDER BY
             time DESC
         LIMIT 10000;
-    "#
-        }
-        Interval::FifteenMinutes => {
-            r#"
-        -- query the materialized realtime view
-        SELECT
-            bucket AS time,
-            open,
-            high,
-            low,
-            close
-        FROM
-            fifteen_minute_candle
-        WHERE
-            pair_id = $1
-            AND
-            bucket <= $2
-        ORDER BY
-            time DESC
-        LIMIT 10000;
-    "#
-        }
-        Interval::OneHour => {
-            r#"
-        -- query the materialized realtime view
-        SELECT
-            bucket AS time,
-            open,
-            high,
-            low,
-            close
-        FROM
-            one_hour_candle
-        WHERE
-            pair_id = $1
-            AND
-            bucket <= $2
-        ORDER BY
-            time DESC
-        LIMIT 10000;
-    "#
-        }
-        Interval::TwoHours => {
-            r#"
-            -- query the materialized realtime view
-        SELECT
-            bucket AS time,
-            open,
-            high,
-            low,
-            close
-        FROM
-            two_hour_candle
-        WHERE
-            pair_id = $1
-            AND
-            bucket <= $2
-        ORDER BY
-            time DESC
-        LIMIT 10000;
-    "#
-        }
-    };
+    "#,
+        get_interval_specifier(interval, false)?
+    );
 
     let date_time = DateTime::from_timestamp(time, 0).ok_or(InfraError::InvalidTimeStamp)?;
 
