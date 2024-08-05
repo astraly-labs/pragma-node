83968fba069323584f327e8588e429e18e7fd221
akhercha <akherchache@pm.me>
2024-08-05 11:35:07 +0200

    feat(pragma_consumer): Main fn

--- pragma-consumer/src/lib.rs
+++ pragma-consumer/src/lib.rs
@@ -6,7 +6,7 @@ pub mod types;
 
 // Re-export of some types so they're publicly accessible through the SDK.
 pub use pragma_common::instrument;
-pub use pragma_common::types::merkle_tree::FeltMerkleProof;
+pub use pragma_common::types::merkle_tree::MerkleProof;
 pub use pragma_common::types::options::{
     Instrument, InstrumentError, OptionCurrency, OptionData, OptionType,
 };
