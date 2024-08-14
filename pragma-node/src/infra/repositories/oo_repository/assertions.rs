e28fa7ad6f9c4659ce72f5766918b05a98da11f1
JordyRo1 <jordy.romu@orange.fr>
2024-08-14 18:38:18 +0200

    fix(oo-api): move unnecesary actions out of conn.interact

--- pragma-node/src/infra/repositories/oo_repository/assertions.rs
+++ pragma-node/src/infra/repositories/oo_repository/assertions.rs
@@ -19,50 +19,52 @@ pub async fn get_assertions(
         .await
         .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
 
-    conn.interact(move |conn| {
-        let mut query = oo_requests::table.into_boxed();
+    let status_clone = status.clone();
 
-        // Apply status filter if provided
-        if let Some(status) = status {
-            match status.as_str() {
-                "settled" => query = query.filter(oo_requests::settled.eq(Some(true))),
-                "disputed" => query = query.filter(oo_requests::disputed.eq(Some(true))),
-                "active" => {
-                    query = query.filter(
-                        oo_requests::settled
-                            .is_null()
-                            .and(oo_requests::disputed.is_null()),
-                    )
+    let results: Vec<OORequest> = conn
+        .interact(move |conn| {
+            let mut query = oo_requests::table.into_boxed();
+
+            if let Some(status) = status_clone {
+                match status.as_str() {
+                    "settled" => query = query.filter(oo_requests::settled.eq(Some(true))),
+                    "disputed" => query = query.filter(oo_requests::disputed.eq(Some(true))),
+                    "active" => {
+                        query = query.filter(
+                            oo_requests::settled
+                                .is_null()
+                                .and(oo_requests::disputed.is_null()),
+                        )
+                    }
+                    _ => {}
                 }
-                _ => {}
-            }
-        };
+            };
 
-        query = query.filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"));
+            query = query.filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"));
 
-        let results: Vec<OORequest> = query
-            .offset(((page - 1) * limit) as i64)
-            .limit(limit as i64)
-            .load(conn)
-            .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
+            query
+                .offset(((page - 1) * limit) as i64)
+                .limit(limit as i64)
+                .load(conn)
+                .map_err(|_| OptimisticOracleError::DatabaseConnection)
+        })
+        .await
+        .map_err(|_| OptimisticOracleError::DatabaseConnection)??;
 
-        let assertions: Vec<Assertion> = results
-            .into_iter()
-            .map(|request| Assertion {
-                assertion_id: request.assertion_id.to_string(),
-                claim: request.claim,
-                bond: request.bond,
-                expiration_time: request.expiration_timestamp,
-                identifier: request.identifier,
-                status: get_status(request.disputed, request.settled),
-                timestamp: request.updated_at,
-            })
-            .collect();
+    let assertions: Vec<Assertion> = results
+        .into_iter()
+        .map(|request| Assertion {
+            assertion_id: request.assertion_id.to_string(),
+            claim: request.claim,
+            bond: request.bond,
+            expiration_time: request.expiration_timestamp,
+            identifier: request.identifier,
+            status: get_status(request.disputed, request.settled),
+            timestamp: request.updated_at,
+        })
+        .collect();
 
-        Ok(assertions)
-    })
-    .await
-    .map_err(|_| OptimisticOracleError::DatabaseConnection)?
+    Ok(assertions)
 }
 
 // Function to get assertion details
@@ -77,38 +79,40 @@ pub async fn get_assertion_details(
 
     let assertion_id = assertion_id.to_string();
 
-    conn.interact(move |conn| {
-        let request: OORequest = oo_requests::table
-            .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
-            .filter(oo_requests::assertion_id.eq(&assertion_id))
-            .first(conn)
-            .map_err(|_| OptimisticOracleError::AssertionDetailsIssue(assertion_id))?;
-
-        let status = get_status(request.disputed, request.settled);
-        Ok(AssertionDetails {
-            assertion: Assertion {
-                assertion_id: request.assertion_id.to_string(),
-                claim: request.claim,
-                bond: request.bond,
-                expiration_time: request.expiration_timestamp,
-                identifier: request.identifier,
-                status,
-                timestamp: request.updated_at,
-            },
-            domain_id: request.domain_id,
-            asserter: request.asserter,
-            disputer: request.disputer.unwrap_or("None".to_string()),
-            disputed: request.disputed.unwrap_or(false),
-            callback_recipient: request.callback_recipient,
-            caller: request.caller,
-            settled: request.settled.unwrap_or(false),
-            settlement_resolution: request.settlement_resolution.into(),
+    let request: OORequest = conn
+        .interact(move |conn| {
+            oo_requests::table
+                .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
+                .filter(oo_requests::assertion_id.eq(&assertion_id))
+                .first(conn)
+                .map_err(|_| OptimisticOracleError::AssertionDetailsIssue(assertion_id))
         })
+        .await
+        .map_err(|_| OptimisticOracleError::DatabaseConnection)??;
+
+    let status = get_status(request.disputed, request.settled);
+    Ok(AssertionDetails {
+        assertion: Assertion {
+            assertion_id: request.assertion_id.to_string(),
+            claim: request.claim,
+            bond: request.bond,
+            expiration_time: request.expiration_timestamp,
+            identifier: request.identifier,
+            status,
+            timestamp: request.updated_at,
+        },
+        domain_id: request.domain_id,
+        asserter: request.asserter,
+        disputer: request.disputer.unwrap_or("None".to_string()),
+        disputed: request.disputed.unwrap_or(false),
+        callback_recipient: request.callback_recipient,
+        caller: request.caller,
+        settled: request.settled.unwrap_or(false),
+        settlement_resolution: request.settlement_resolution.into(),
     })
-    .await
-    .map_err(|_| OptimisticOracleError::DatabaseConnection)?
 }
 
+// Function to get disputed assertions
 pub async fn get_disputed_assertions(
     onchain_pool: &deadpool_diesel::postgres::Pool,
     page: u32,
@@ -119,45 +123,44 @@ pub async fn get_disputed_assertions(
         .await
         .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
 
-    conn.interact(move |conn| {
-        let query = oo_requests::table
-            .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
-            .filter(oo_requests::disputed.eq(true))
-            .offset(((page - 1) * limit) as i64)
-            .limit(limit as i64);
+    let results: Vec<OORequest> = conn
+        .interact(move |conn| {
+            let query = oo_requests::table
+                .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
+                .filter(oo_requests::disputed.eq(true))
+                .offset(((page - 1) * limit) as i64)
+                .limit(limit as i64);
 
-        let results: Vec<OORequest> = query
-            .load(conn)
-            .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
+            query
+                .load(conn)
+                .map_err(|_| OptimisticOracleError::DatabaseConnection)
+        })
+        .await
+        .map_err(|_| OptimisticOracleError::DatabaseConnection)??;
 
-        let disputed_assertions: Result<Vec<DisputedAssertion>, OptimisticOracleError> = results
-            .into_iter()
-            .map(|request| {
-                let disputer = request.disputer.ok_or_else(|| {
-                    OptimisticOracleError::DisputerNotSet(request.assertion_id.clone())
-                })?;
+    results
+        .into_iter()
+        .map(|request| {
+            let disputer = request.disputer.ok_or_else(|| {
+                OptimisticOracleError::DisputerNotSet(request.assertion_id.clone())
+            })?;
 
-                Ok(DisputedAssertion {
-                    assertion: Assertion {
-                        assertion_id: request.assertion_id.to_string(),
-                        claim: request.claim,
-                        bond: request.bond,
-                        expiration_time: request.expiration_timestamp,
-                        identifier: request.identifier,
-                        status: Status::Disputed,
-                        timestamp: request.updated_at,
-                    },
-                    disputer,
-                    disputed_at: request.updated_at,
-                    disputed_tx: request.updated_at_tx,
-                })
+            Ok(DisputedAssertion {
+                assertion: Assertion {
+                    assertion_id: request.assertion_id.to_string(),
+                    claim: request.claim,
+                    bond: request.bond,
+                    expiration_time: request.expiration_timestamp,
+                    identifier: request.identifier,
+                    status: Status::Disputed,
+                    timestamp: request.updated_at,
+                },
+                disputer,
+                disputed_at: request.updated_at,
+                disputed_tx: request.updated_at_tx,
             })
-            .collect();
-
-        disputed_assertions
-    })
-    .await
-    .map_err(|_| OptimisticOracleError::DatabaseConnection)?
+        })
+        .collect()
 }
 
 // Function to get resolved assertions
@@ -171,47 +174,46 @@ pub async fn get_resolved_assertions(
         .await
         .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
 
-    conn.interact(move |conn| {
-        let query = oo_requests::table
-            .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
-            .filter(oo_requests::settled.eq(true))
-            .offset(((page - 1) * limit) as i64)
-            .limit(limit as i64);
+    let results: Vec<OORequest> = conn
+        .interact(move |conn| {
+            let query = oo_requests::table
+                .filter(diesel::dsl::sql::<Bool>("upper(_cursor) IS NULL"))
+                .filter(oo_requests::settled.eq(true))
+                .offset(((page - 1) * limit) as i64)
+                .limit(limit as i64);
 
-        let results: Vec<OORequest> = query
-            .load(conn)
-            .map_err(|_| OptimisticOracleError::DatabaseConnection)?;
+            query
+                .load(conn)
+                .map_err(|_| OptimisticOracleError::DatabaseConnection)
+        })
+        .await
+        .map_err(|_| OptimisticOracleError::DatabaseConnection)??;
 
-        let resolved_assertions: Result<Vec<ResolvedAssertion>, OptimisticOracleError> = results
-            .into_iter()
-            .map(|request| {
-                let settled_address = request.settle_caller.ok_or_else(|| {
-                    OptimisticOracleError::SettlerNotSet(request.assertion_id.clone())
-                })?;
+    results
+        .into_iter()
+        .map(|request| {
+            let settled_address = request.settle_caller.ok_or_else(|| {
+                OptimisticOracleError::SettlerNotSet(request.assertion_id.clone())
+            })?;
 
-                Ok(ResolvedAssertion {
-                    assertion: Assertion {
-                        assertion_id: request.assertion_id,
-                        claim: request.claim,
-                        bond: request.bond,
-                        expiration_time: request.expiration_timestamp,
-                        identifier: request.identifier,
-                        status: Status::Settled,
-                        timestamp: request.updated_at,
-                    },
-                    settled_address,
-                    settlement_resolution: request.settlement_resolution.into(),
-                    disputed: request.disputed.unwrap_or(false),
-                    settled_at: request.updated_at,
-                    settlement_tx: request.updated_at_tx,
-                })
+            Ok(ResolvedAssertion {
+                assertion: Assertion {
+                    assertion_id: request.assertion_id,
+                    claim: request.claim,
+                    bond: request.bond,
+                    expiration_time: request.expiration_timestamp,
+                    identifier: request.identifier,
+                    status: Status::Settled,
+                    timestamp: request.updated_at,
+                },
+                settled_address,
+                settlement_resolution: request.settlement_resolution.into(),
+                disputed: request.disputed.unwrap_or(false),
+                settled_at: request.updated_at,
+                settlement_tx: request.updated_at_tx,
             })
-            .collect();
-
-        resolved_assertions
-    })
-    .await
-    .map_err(|_| OptimisticOracleError::DatabaseConnection)?
+        })
+        .collect()
 }
 
 fn get_status(disputed: Option<bool>, settled: Option<bool>) -> Status {
