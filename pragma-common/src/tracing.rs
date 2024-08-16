4d44c438240ebde9293d67472885bd4ea2fdfd25
0xevolve <Artevolve@yahoo.com>
2024-08-16 13:31:10 +0100

    feat: axum tracing layer

--- pragma-common/src/tracing.rs
+++ pragma-common/src/tracing.rs
@@ -2,6 +2,11 @@ use tracing_subscriber::filter::filter_fn;
 use tracing_subscriber::prelude::*;
 
 pub fn init_tracing(service_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let axum_layer = tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        // axum logs rejections from built-in extractors with the `axum::rejection`
+        // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+        "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
+    });
     let axiom_layer = tracing_axiom::builder_with_env(service_name)?
         .with_dataset("pragma-node")?
         .build()?;
@@ -23,7 +28,8 @@ pub fn init_tracing(service_name: &str) -> Result<(), Box<dyn std::error::Error>
 
     tracing_subscriber::registry()
         .with(fmt_layer.with_filter(filter.clone()))
-        .with(axiom_layer.with_filter(filter))
+        .with(axiom_layer.with_filter(filter.clone()))
+        .with(axum_layer.with_filter(filter))
         .try_init()?;
 
     Ok(())
