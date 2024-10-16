8489ff05024f7a6ef0ca2caa0b3dc5d7e50ca9e9
0xevolve <Artevolve@yahoo.com>
2024-10-16 19:05:53 +0100

    feat: add new indexes

--- infra/pragma-node/postgres_migrations/15-create-indexes.sql
+++ infra/pragma-node/postgres_migrations/15-create-indexes.sql
@@ -17,3 +17,26 @@ CREATE INDEX spot_idx_publisher_timestamp ON spot_entry (publisher, timestamp);
 CREATE INDEX mainnet_spot_idx_publisher_timestamp ON mainnet_spot_entry (publisher, timestamp);
 CREATE INDEX future_idx_publisher_timestamp ON future_entry (publisher, timestamp);
 CREATE INDEX mainnet_future_idx_publisher_timestamp ON mainnet_future_entry (publisher, timestamp);
+
+CREATE INDEX mainnet_idx_pair_id_source ON mainnet_spot_entry (pair_id, source);
+CREATE INDEX mainnet_idx_block_timestamp ON mainnet_spot_entry (block_timestamp DESC);
+CREATE INDEX idx_pair_id_source ON spot_entry (pair_id, source);
+CREATE INDEX idx_block_timestamp ON spot_entry (block_timestamp DESC);
+CREATE INDEX pragma_devnet_idx_pair_id_source ON pragma_devnet_spot_entry (pair_id, source);
+CREATE INDEX pragma_devnet_idx_block_timestamp ON pragma_devnet_spot_entry (block_timestamp DESC);
+
+CREATE INDEX mainnet_future_idx_pair_id_source ON mainnet_future_entry (pair_id, source);
+CREATE INDEX mainnet_future_idx_block_timestamp ON mainnet_future_entry (block_timestamp DESC);
+CREATE INDEX future_idx_pair_id_source ON future_entry (pair_id, source);
+CREATE INDEX future_idx_block_timestamp ON future_entry (block_timestamp DESC);
+CREATE INDEX pragma_devnet_future_idx_pair_id_source ON pragma_devnet_future_entry (pair_id, source);
+CREATE INDEX pragma_devnet_future_idx_block_timestamp ON pragma_devnet_future_entry (block_timestamp DESC);
+
+
+
+
+
+
+
+
+
