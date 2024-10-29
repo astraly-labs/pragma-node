8876b8fe5e06e3807be0539927f38f61f5f34b64
azurwastaken <caseaux.maxime@gmail.com>
2024-10-29 18:57:10 +0100

    Update up.sql

--- pragma-entities/migrations/2024-10-25-092708_add_new_pairs/up.sql
+++ pragma-entities/migrations/2024-10-25-092708_add_new_pairs/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 INSERT INTO public.currencies (name, decimals, abstract, ethereum_address) VALUES
-('TON', 9, false, "0x582d872a1b094fc48f5de31d3b73f2d9be47def1"),
+('TON', 9, false, '0x582D872A1B094FC48F5DE31D3B73F2D9BE47DEF1'),
 ('JTO', 9, false, NULL),
-('OKB', 18, false, "0x75231f58b43240c9718dd58b4967c5114342a86c"),
-('1000SATS', 8, false, NULL),
+('OKB', 18, false, '0x75231F58B43240C9718DD58B4967C5114342A86C'),
+('1000SATS', 8, false, NULL);
