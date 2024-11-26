cf371864bacb5283ac059722048fbf382e2820f6
akhercha <akherchache@pm.me>
2024-11-26 09:39:10 +0100

    dev(better_logs): minor stuff

--- pragma-node/src/main.rs
+++ pragma-node/src/main.rs
@@ -29,7 +29,6 @@ pub struct AppState {
     offchain_pool: Pool,
     onchain_pool: Pool,
     // Redis connection
-    #[allow(dead_code)]
     redis_client: Option<Arc<redis::Client>>,
     // Database caches
     caches: Arc<CacheRegistry>,
@@ -55,6 +54,7 @@ impl fmt::Debug for AppState {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    // We export our telemetry - so we can monitor the API through Signoz.
     let otel_endpoint = std::env::var("OTEL_EXPORTER_OTLP_ENDPOINT")
         .unwrap_or_else(|_| "http://signoz.dev.pragma.build:4317".to_string());
     pragma_common::telemetry::init_telemetry("pragma-node".into(), otel_endpoint, None)?;
@@ -83,7 +83,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Init the redis client - Optionnal, only for endpoints that interact with Redis,
     // i.e just the Merkle Feeds endpoint for now.
-    // TODO(akhercha): See with Hithem for production mode
     let redis_client = match pragma_entities::connection::init_redis_client(
         config.redis_host(),
         config.redis_port(),
