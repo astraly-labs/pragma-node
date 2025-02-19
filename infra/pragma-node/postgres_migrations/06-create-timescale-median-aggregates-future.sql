e85121d1111b4ff6d1c15954e5375d34ac23de9d
Yasser Tahiri <yasserth19@gmail.com>
2025-02-19 01:05:59 +0100

    ♻️ replace `approx_percentile` with `perentile_disc`

--- infra/pragma-node/postgres_migrations/06-create-timescale-median-aggregates-future.sql
+++ infra/pragma-node/postgres_migrations/06-create-timescale-median-aggregates-future.sql
@@ -1,9 +1,10 @@
+-- 10 seconds aggregation
 CREATE MATERIALIZED VIEW future_price_10_s_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS SELECT 
     pair_id,
     time_bucket('10 seconds'::interval, timestamp) as bucket,
-    approx_percentile(0.5, percentile_agg(price))::numeric AS median_price,
+    percentile_disc(0.5) WITHIN GROUP (ORDER BY price)::numeric AS median_price,
     COUNT(DISTINCT source) as num_sources
 FROM future_entry
 GROUP BY bucket, pair_id
@@ -14,12 +15,13 @@ SELECT add_continuous_aggregate_policy('future_price_10_s_agg',
   end_offset => INTERVAL '10 seconds',
   schedule_interval => INTERVAL '10 seconds');
 
+-- 1 minute aggregation
 CREATE MATERIALIZED VIEW future_price_1_min_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS SELECT 
     pair_id,
     time_bucket('1 min'::interval, timestamp) as bucket,
-    approx_percentile(0.5, percentile_agg(price))::numeric AS median_price,
+    percentile_disc(0.5) WITHIN GROUP (ORDER BY price)::numeric AS median_price,
     COUNT(DISTINCT source) as num_sources
 FROM future_entry
 GROUP BY bucket, pair_id
@@ -30,13 +32,13 @@ SELECT add_continuous_aggregate_policy('future_price_1_min_agg',
   end_offset => INTERVAL '1 min',
   schedule_interval => INTERVAL '1 min');
 
-
+-- 15 minutes aggregation
 CREATE MATERIALIZED VIEW future_price_15_min_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS SELECT 
     pair_id,
     time_bucket('15 min'::interval, timestamp) as bucket,
-    approx_percentile(0.5, percentile_agg(price))::numeric AS median_price,
+    percentile_disc(0.5) WITHIN GROUP (ORDER BY price)::numeric AS median_price,
     COUNT(DISTINCT source) as num_sources
 FROM future_entry
 GROUP BY bucket, pair_id
@@ -47,13 +49,13 @@ SELECT add_continuous_aggregate_policy('future_price_15_min_agg',
   end_offset => INTERVAL '15 min',
   schedule_interval => INTERVAL '15 min');
 
-
+-- 1 hour aggregation
 CREATE MATERIALIZED VIEW future_price_1_hour_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS SELECT 
     pair_id,
     time_bucket('1 hour'::interval, timestamp) as bucket,
-    approx_percentile(0.5, percentile_agg(price))::numeric AS median_price,
+    percentile_disc(0.5) WITHIN GROUP (ORDER BY price)::numeric AS median_price,
     COUNT(DISTINCT source) as num_sources
 FROM future_entry
 GROUP BY bucket, pair_id
@@ -64,13 +66,13 @@ SELECT add_continuous_aggregate_policy('future_price_1_hour_agg',
   end_offset => INTERVAL '1 hour',
   schedule_interval => INTERVAL '1 hour');
 
-
+-- 2 hours aggregation
 CREATE MATERIALIZED VIEW future_price_2_hour_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS SELECT 
     pair_id,
     time_bucket('2 hour'::interval, timestamp) as bucket,
-    approx_percentile(0.5, percentile_agg(price))::numeric AS median_price,
+    percentile_disc(0.5) WITHIN GROUP (ORDER BY price)::numeric AS median_price,
     COUNT(DISTINCT source) as num_sources
 FROM future_entry
 GROUP BY bucket, pair_id
