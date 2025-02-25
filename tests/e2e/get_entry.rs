b2c2e4d84ba026f320f7db113ed2503bf81616bc
Remsko <pinoit.remi@gmail.com>
2025-02-25 17:13:42 +0100

    tests: more tests

--- tests/e2e/get_entry.rs
+++ tests/e2e/get_entry.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     assert_hex_prices_within_threshold,
-    common::{constants::VARIATION_PERCENTAGE, setup::{setup_containers, TestHelper}},
+    common::{constants::VARIATION_PERCENTAGE, setup::{setup_containers, TestHelper}, utils::populate::get_pair_price},
 };
 
 use crate::common::utils::populate;
@@ -99,72 +99,6 @@ pub struct GetEntryResponse {
 #[case::one_week(Interval::OneWeek)]
 #[serial_test::serial]
 #[tokio::test]
-async fn get_entry_median_ok(
-    #[future] setup_containers: TestHelper,
-    #[case] queried_interval: Interval,
-) {
-    let mut hlpr = setup_containers.await;
-
-    // TODO: Insert way MORE entries, maybe we should have an utils to generate
-    // random fake data with some constraints?
-    // 1. Insert one entry
-    let pair_id = "ETH/USD";
-    let current_timestamp: u64 = chrono::Utc::now().timestamp() as u64;
-    let price: u128 = 2705_53000000; // 8 decimals
-    let sql = populate::entry_from(pair_id, current_timestamp, price, "BINANCE");
-    hlpr.execute_sql(&hlpr.offchain_pool, sql).await;
-
-    let queried_aggregation = AggregationMode::Median;
-
-    // 2. Refresh the timescale view
-    hlpr.refresh_offchain_continuous_aggregate(
-        current_timestamp,
-        queried_interval,
-        queried_aggregation,
-    )
-    .await;
-
-    // 3. Call the endpoint
-    let endpoint = get_entry_endpoint(
-        "ETH",
-        "USD",
-        GetEntryRequestParams::new()
-            .with_timestamp(current_timestamp)
-            .with_interval(queried_interval)
-            .with_routing(false)
-            .with_aggregation(queried_aggregation),
-    );
-    tracing::info!("with endpoint: {endpoint}");
-
-    let response = reqwest::get(hlpr.endpoint(&endpoint))
-        .await
-        .expect("Error while fetching data from pragma node")
-        .json::<GetEntryResponse>()
-        .await
-        .expect("Could not retrieve a valid GetEntryResponse");
-
-    hlpr.shutdown_local_pragma_node().await;
-
-    // 4. Assert
-    let expected_price_hex = format!("0x{price:x}");
-
-    let threshold = BigDecimal::from_f64(1.0).unwrap();
-    // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
-    // So we just check if the price we have is in ~1% bonds.
-    assert_hex_prices_within_threshold!(&response.price, &expected_price_hex, threshold);
-}
-
-#[rstest]
-#[case::one_second(Interval::OneSecond)]
-#[case::five_seconds(Interval::FiveSeconds)]
-#[case::one_minute(Interval::OneMinute)]
-#[case::fifteen_minutes(Interval::FifteenMinutes)]
-#[case::one_hour(Interval::OneHour)]
-#[case::two_hours(Interval::TwoHours)]
-#[case::one_day(Interval::OneDay)]
-#[case::one_week(Interval::OneWeek)]
-#[serial_test::serial]
-#[tokio::test]
 async fn get_entry_median_ok_many(
     #[future] setup_containers: TestHelper,
     #[case] queried_interval: Interval,
@@ -222,80 +156,8 @@ async fn get_entry_median_ok_many(
 }
 
 #[rstest]
-#[case::one_second(Interval::OneSecond)]
-#[case::five_seconds(Interval::FiveSeconds)]
-#[case::one_minute(Interval::OneMinute)]
-#[case::fifteen_minutes(Interval::FifteenMinutes)]
 #[case::one_hour(Interval::OneHour)]
 #[case::two_hours(Interval::TwoHours)]
-#[case::one_day(Interval::OneDay)]
-#[case::one_week(Interval::OneWeek)]
-#[serial_test::serial]
-#[tokio::test]
-async fn get_entry_twap_ok(
-    #[future] setup_containers: TestHelper,
-    #[case] queried_interval: Interval,
-) {
-    let mut hlpr = setup_containers.await;
-
-    // 1. Insert one entry
-    let pair_id = "STRK/USD";
-    let current_timestamp: u64 = chrono::Utc::now().timestamp() as u64;
-    let price: u128 = 2705_53000000; // 8 decimals
-    let sql = populate::entry_from(pair_id, current_timestamp, price, "BINANCE");
-    hlpr.execute_sql(&hlpr.offchain_pool, sql).await;
-
-    let queried_aggregation = AggregationMode::Twap;
-
-    // 2. Refresh the timescale view
-    hlpr.refresh_offchain_continuous_aggregate(
-        current_timestamp,
-        queried_interval,
-        queried_aggregation,
-    )
-    .await;
-
-    // 3. Call the endpoint
-    let endpoint = get_entry_endpoint(
-        "STRK",
-        "USD",
-        GetEntryRequestParams::new()
-            .with_timestamp(current_timestamp)
-            .with_interval(queried_interval)
-            .with_routing(false)
-            .with_aggregation(queried_aggregation),
-    );
-    tracing::info!("with endpoint: {endpoint}");
-
-    let response = reqwest::get(hlpr.endpoint(&endpoint))
-        .await
-        .expect("Error while fetching data from pragma node");
-
-    let response = response
-        .json::<GetEntryResponse>()
-        .await
-        .expect("Could not retrieve a valid GetEntryResponse");
-
-    hlpr.shutdown_local_pragma_node().await;
-
-    // 4. Assert
-    let expected_price_hex = format!("0x{price:x}");
-
-    let threshold = BigDecimal::from_f64(VARIATION_PERCENTAGE).unwrap();
-    // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
-    // So we just check if the price we have is in ~1% bonds.
-    assert_hex_prices_within_threshold!(&response.price, &expected_price_hex, threshold);
-}
-
-#[rstest]
-#[case::one_second(Interval::OneSecond)]
-#[case::five_seconds(Interval::FiveSeconds)]
-#[case::one_minute(Interval::OneMinute)]
-#[case::fifteen_minutes(Interval::FifteenMinutes)]
-#[case::one_hour(Interval::OneHour)]
-#[case::two_hours(Interval::TwoHours)]
-#[case::one_day(Interval::OneDay)]
-#[case::one_week(Interval::OneWeek)]
 #[serial_test::serial]
 #[tokio::test]
 async fn get_entry_twap_many_ok(
@@ -347,6 +209,155 @@ async fn get_entry_twap_many_ok(
 
     // 4. Assert
     let expected_price_hex = format!("0x{price:x}");
+
+    let threshold = BigDecimal::from_f64(VARIATION_PERCENTAGE).unwrap();
+    // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
+    // So we just check if the price we have is in ~1% bonds.
+    assert_hex_prices_within_threshold!(&response.price, &expected_price_hex, threshold);
+}
+
+#[rstest]
+#[case::one_hour(Interval::OneHour)]
+#[case::two_hours(Interval::TwoHours)]
+#[serial_test::serial]
+#[tokio::test]
+async fn get_entry_twap_routing_many_ok(
+    #[future] setup_containers: TestHelper,
+    #[case] queried_interval: Interval,
+) {
+    let mut hlpr = setup_containers.await;
+
+    // 1. Insert one entry
+    let pair_id = "STRK/USD";
+    let current_timestamp: u64 = chrono::Utc::now().timestamp() as u64;
+    let price: u128 = populate::get_pair_price(pair_id); // 8 decimals
+    let sql = populate::entry_from(pair_id, current_timestamp, price, "BINANCE");
+    hlpr.execute_sql(&hlpr.offchain_pool, sql).await;
+
+    let queried_aggregation = AggregationMode::Twap;
+
+    // 2. Refresh the timescale view
+    hlpr.refresh_offchain_continuous_aggregate(
+        current_timestamp,
+        queried_interval,
+        queried_aggregation,
+    )
+    .await;
+
+    // 3. Call the endpoint
+    let endpoint = get_entry_endpoint(
+        "STRK",
+        "USD",
+        GetEntryRequestParams::new()
+            .with_timestamp(current_timestamp)
+            .with_interval(queried_interval)
+            .with_routing(false)
+            .with_aggregation(queried_aggregation),
+    );
+    tracing::info!("with endpoint: {endpoint}");
+
+    let response = reqwest::get(hlpr.endpoint(&endpoint))
+        .await
+        .expect("Error while fetching data from pragma node");
+
+    let response = response
+        .json::<GetEntryResponse>()
+        .await
+        .expect("Could not retrieve a valid GetEntryResponse");
+
+
+    hlpr.shutdown_local_pragma_node().await;
+
+    // 4. Assert
+    let expected_price_hex = format!("0x{price:x}");
+
+    let threshold = BigDecimal::from_f64(VARIATION_PERCENTAGE).unwrap();
+    // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
+    // So we just check if the price we have is in ~1% bonds.
+    assert_hex_prices_within_threshold!(&response.price, &expected_price_hex, threshold);
+
+    let endpoint = get_entry_endpoint(
+        "STRK",
+        "USD",
+        GetEntryRequestParams::new()
+            .with_timestamp(current_timestamp)
+            .with_interval(queried_interval)
+            .with_routing(true)
+            .with_aggregation(queried_aggregation),
+    );
+    tracing::info!("with endpoint: {endpoint}");
+
+    let response_routing = reqwest::get(hlpr.endpoint(&endpoint))
+        .await
+        .expect("Error while fetching data from pragma node");
+
+    let response_routing = response_routing
+        .json::<GetEntryResponse>()
+        .await
+        .expect("Could not retrieve a valid GetEntryResponse");
+
+    let threshold = BigDecimal::from_f64(VARIATION_PERCENTAGE).unwrap();
+    // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
+    // So we just check if the price we have is in ~1% bonds.
+    assert_hex_prices_within_threshold!(&response.price, &expected_price_hex, threshold);
+
+    assert!(response.price == response_routing.price);
+}
+
+#[rstest]
+#[case::two_hours(Interval::TwoHours)]
+#[serial_test::serial]
+#[tokio::test]
+async fn get_entry_twap_2hours_strk_eth_ok(
+    #[future] setup_containers: TestHelper,
+    #[case] queried_interval: Interval,
+) {
+    let mut hlpr = setup_containers.await;
+
+    // 1. Insert one entry
+    let pair_id = "STRK/USD";
+    let current_timestamp: u64 = chrono::Utc::now().timestamp() as u64;
+    let price: u128 = populate::get_pair_price(pair_id);
+    let sql_many = populate::generate_entries(1000, current_timestamp);
+
+    hlpr.execute_sql_many(&hlpr.offchain_pool, sql_many).await;
+
+    let queried_aggregation = AggregationMode::Twap;
+
+    // 2. Refresh the timescale view
+    hlpr.refresh_offchain_continuous_aggregate(
+        current_timestamp,
+        queried_interval,
+        queried_aggregation,
+    )
+    .await;
+
+    // 3. Call the endpoint
+    let endpoint = get_entry_endpoint(
+        "STRK",
+        "ETH",
+        GetEntryRequestParams::new()
+            .with_timestamp(current_timestamp)
+            .with_interval(queried_interval)
+            .with_routing(true)
+            .with_aggregation(queried_aggregation),
+    );
+    tracing::info!("with endpoint: {endpoint}");
+
+    let response = reqwest::get(hlpr.endpoint(&endpoint))
+        .await
+        .expect("Error while fetching data from pragma node");
+
+    let response = response
+        .json::<GetEntryResponse>()
+        .await
+        .expect("Could not retrieve a valid GetEntryResponse");
+
+    hlpr.shutdown_local_pragma_node().await;
+
+    // 4. Assert
+    let strk_eth_price = price / get_pair_price("ETH/USD");
+    let expected_price_hex = format!("0x{strk_eth_price:x}");
 
     let threshold = BigDecimal::from_f64(VARIATION_PERCENTAGE).unwrap();
     // NOTE: approx_percentile of timescaledb returns an approximative value for the median.
