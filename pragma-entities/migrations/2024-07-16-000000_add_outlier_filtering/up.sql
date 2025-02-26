e2462dca23a31e8849baf509f49d9a6fd6869877
akhercha <akherchache@pm.me>
2025-02-26 14:02:59 +0100

    tests(e2e_get_entry): Fixed deadlock

--- pragma-entities/migrations/2024-07-16-000000_add_outlier_filtering/up.sql
+++ pragma-entities/migrations/2024-07-16-000000_add_outlier_filtering/up.sql
@@ -1,15 +1,22 @@
--- Drop dependent views first (in reverse order)
+-- Migration Part 1: Configuration settings to reduce deadlocks
+-- Set higher lock timeout and disable background workers
+SET lock_timeout = '60s';
+SET timescaledb.enable_maintenance_worker = 'off';
+
+-- Take explicit locks on tables we'll be modifying
+LOCK TABLE entries IN ACCESS EXCLUSIVE MODE;
+
+-- Migration Part 2: Drop all existing views in reverse dependency order
 DROP MATERIALIZED VIEW IF EXISTS new_1_week_candle;
 DROP MATERIALIZED VIEW IF EXISTS new_1_day_candle;
 DROP MATERIALIZED VIEW IF EXISTS new_1_h_candle;
 DROP MATERIALIZED VIEW IF EXISTS new_15_min_candle;
 DROP MATERIALIZED VIEW IF EXISTS new_5_min_candle;
 DROP MATERIALIZED VIEW IF EXISTS new_1_min_candle;
-
--- Drop and recreate the base 10s aggregate with source filtering
 DROP MATERIALIZED VIEW IF EXISTS price_10_s_agg;
+DROP VIEW IF EXISTS filtered_price_10_s_agg;
 
--- Create a materialized view for the base 10s aggregation
+-- Migration Part 3: Create the base 10s aggregate
 CREATE MATERIALIZED VIEW price_10_s_agg
 WITH (timescaledb.continuous, timescaledb.materialized_only = false)
 AS 
@@ -27,11 +34,6 @@ GROUP BY bucket, pair_id
 HAVING COUNT(DISTINCT source) > 2  -- Only consider buckets with more than 2 sources
 WITH NO DATA;
 
-SELECT add_continuous_aggregate_policy('price_10_s_agg',
-  start_offset => INTERVAL '1 day',
-  end_offset => INTERVAL '10 seconds',
-  schedule_interval => INTERVAL '10 seconds');
-
 -- Create a view for outlier filtering using standard deviation method
 CREATE OR REPLACE VIEW filtered_price_10_s_agg AS
 SELECT 
@@ -43,7 +45,7 @@ FROM price_10_s_agg
 WHERE median_price >= (avg_price - 2 * stddev_price)  -- Filter lower outliers
     AND median_price <= (avg_price + 2 * stddev_price);  -- Filter upper outliers
 
--- Recreate all the OHLC views using the base continuous aggregate
+-- Migration Part 4: Create dependent materialized views
 CREATE MATERIALIZED VIEW new_1_min_candle
 WITH (timescaledb.continuous) AS
     SELECT
@@ -56,11 +58,6 @@ WITH (timescaledb.continuous) AS
     FROM price_10_s_agg
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
-
-SELECT add_continuous_aggregate_policy('new_1_min_candle',
-    start_offset => INTERVAL '3 minutes',
-    end_offset => INTERVAL '1 minute',
-    schedule_interval => INTERVAL '1 minute');
 
 CREATE MATERIALIZED VIEW new_5_min_candle
 WITH (timescaledb.continuous) AS
@@ -75,11 +72,6 @@ WITH (timescaledb.continuous) AS
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
 
-SELECT add_continuous_aggregate_policy('new_5_min_candle',
-    start_offset => INTERVAL '15 minutes',
-    end_offset => INTERVAL '5 minutes',
-    schedule_interval => INTERVAL '5 minutes');
-
 CREATE MATERIALIZED VIEW new_15_min_candle
 WITH (timescaledb.continuous) AS
     SELECT
@@ -92,11 +84,6 @@ WITH (timescaledb.continuous) AS
     FROM price_10_s_agg
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
-
-SELECT add_continuous_aggregate_policy('new_15_min_candle',
-    start_offset => INTERVAL '45 minutes',
-    end_offset => INTERVAL '15 minutes',
-    schedule_interval => INTERVAL '15 minutes');
 
 CREATE MATERIALIZED VIEW new_1_h_candle
 WITH (timescaledb.continuous) AS
@@ -111,11 +98,6 @@ WITH (timescaledb.continuous) AS
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
 
-SELECT add_continuous_aggregate_policy('new_1_h_candle',
-    start_offset => INTERVAL '3 hours',
-    end_offset => INTERVAL '1 hour',
-    schedule_interval => INTERVAL '1 hour');
-
 CREATE MATERIALIZED VIEW new_1_day_candle
 WITH (timescaledb.continuous) AS
     SELECT
@@ -128,11 +110,6 @@ WITH (timescaledb.continuous) AS
     FROM price_10_s_agg
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
-
-SELECT add_continuous_aggregate_policy('new_1_day_candle',
-    start_offset => INTERVAL '3 days',
-    end_offset => INTERVAL '1 day',
-    schedule_interval => INTERVAL '1 day');
 
 CREATE MATERIALIZED VIEW new_1_week_candle
 WITH (timescaledb.continuous) AS
@@ -147,7 +124,42 @@ WITH (timescaledb.continuous) AS
     GROUP BY ohlc_bucket, pair_id
     WITH NO DATA;
 
+-- Migration Part 5: Add continuous aggregate policies 
+-- (Separated from view creation to avoid conflicts)
+SELECT add_continuous_aggregate_policy('price_10_s_agg',
+  start_offset => INTERVAL '1 day',
+  end_offset => INTERVAL '10 seconds',
+  schedule_interval => INTERVAL '10 seconds');
+
+SELECT add_continuous_aggregate_policy('new_1_min_candle',
+    start_offset => INTERVAL '3 minutes',
+    end_offset => INTERVAL '1 minute',
+    schedule_interval => INTERVAL '1 minute');
+
+SELECT add_continuous_aggregate_policy('new_5_min_candle',
+    start_offset => INTERVAL '15 minutes',
+    end_offset => INTERVAL '5 minutes',
+    schedule_interval => INTERVAL '5 minutes');
+
+SELECT add_continuous_aggregate_policy('new_15_min_candle',
+    start_offset => INTERVAL '45 minutes',
+    end_offset => INTERVAL '15 minutes',
+    schedule_interval => INTERVAL '15 minutes');
+
+SELECT add_continuous_aggregate_policy('new_1_h_candle',
+    start_offset => INTERVAL '3 hours',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+SELECT add_continuous_aggregate_policy('new_1_day_candle',
+    start_offset => INTERVAL '3 days',
+    end_offset => INTERVAL '1 day',
+    schedule_interval => INTERVAL '1 day');
+
 SELECT add_continuous_aggregate_policy('new_1_week_candle',
     start_offset => INTERVAL '3 week',
     end_offset => INTERVAL '1 week',
-    schedule_interval => INTERVAL '1 week'); 
+    schedule_interval => INTERVAL '1 week');
+
+-- Migration Part 6: Re-enable maintenance worker
+SET timescaledb.enable_maintenance_worker = 'on';
