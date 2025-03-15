72195af30066b3e1eae769946f074e1e4497835e
0xevolve <Artevolve@yahoo.com>
2025-03-15 12:53:54 +0800

    feat: compression migration using hypercore

--- pragma-entities/migrations/2024-01-12-135355_add_candlestick_views/up.sql
+++ pragma-entities/migrations/2024-01-12-135355_add_candlestick_views/up.sql
@@ -29,6 +29,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Spot entries candlesticks
+SELECT create_candlestick_view('candle_10_s', '10 seconds'::interval, '30 seconds'::interval, 'price_1_s_agg');
 SELECT create_candlestick_view('candle_1_min', '1 minute'::interval, '3 minutes'::interval, 'price_1_s_agg');
 SELECT create_candlestick_view('candle_5_min', '5 minutes'::interval, '15 minutes'::interval, 'price_10_s_agg');
 SELECT create_candlestick_view('candle_15_min', '15 minutes'::interval, '45 minutes'::interval, 'price_10_s_agg');
@@ -36,6 +37,7 @@ SELECT create_candlestick_view('candle_1_h', '1 hour'::interval, '3 hours'::inte
 SELECT create_candlestick_view('candle_1_day', '1 day'::interval, '3 days'::interval, 'price_10_s_agg');
 
 -- Future entries candlesticks
+SELECT create_candlestick_view('candle_10_s_future', '10 seconds'::interval, '30 seconds'::interval, 'price_1_s_agg_future');
 SELECT create_candlestick_view('candle_1_min_future', '1 minute'::interval, '3 minutes'::interval, 'price_1_s_agg_future');
 SELECT create_candlestick_view('candle_5_min_future', '5 minutes'::interval, '15 minutes'::interval, 'price_10_s_agg_future');
 SELECT create_candlestick_view('candle_15_min_future', '15 minutes'::interval, '45 minutes'::interval, 'price_10_s_agg_future');
