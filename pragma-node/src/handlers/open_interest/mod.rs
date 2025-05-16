a6b40fed553db80add89450c56862cbb2c1a0ff6
0xevolve <Artevolve@yahoo.com>
2025-05-16 13:19:22 +0100

    feat: add oi routes

--- pragma-node/src/handlers/open_interest/mod.rs
+++ pragma-node/src/handlers/open_interest/mod.rs
@@ -1,3 +1,7 @@
 pub mod get_historical_open_interest;
 pub mod get_instruments;
 pub mod get_open_interest;
+
+pub use get_historical_open_interest::*;
+pub use get_instruments::*;
+pub use get_open_interest::*;
