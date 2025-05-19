97173caec208ce818e4c2c294ddd4605c86965fe
Yasser Tahiri <yasserth19@gmail.com>
2025-05-19 18:12:31 +0100

    ♻️ fix the migration issue (#238)
    * :recycle: fix the migration issue
    
    * :recycle: Creates the indexes with `IF NOT EXISTS` clauses

--- pragma-entities/migrations/2025-05-19-141340_add-indexes-fr-oi-tables/down.sql
+++ pragma-entities/migrations/2025-05-19-141340_add-indexes-fr-oi-tables/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-DROP INDEX idx_funding_rates_pair_source_ts;
-DROP INDEX idx_open_interest_pair_source_ts;
+DROP INDEX IF EXISTS idx_funding_rates_pair_source_ts;
+DROP INDEX IF EXISTS idx_open_interest_pair_source_ts; 
