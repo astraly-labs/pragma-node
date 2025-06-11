a8fb8de69015e02c32631f8d8dbaaafcd057be51
0xevolve <Artevolve@yahoo.com>
2025-06-11 13:50:43 +0200

    fix: switch to fallback provider

--- pragma-node/src/infra/rpc/mod.rs
+++ pragma-node/src/infra/rpc/mod.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, LazyLock},
-};
+use std::{collections::HashMap, sync::LazyLock};
 
 use starknet::providers::Provider;
 use starknet::{
@@ -10,18 +7,20 @@ use starknet::{
         utils::{cairo_short_string_to_felt, get_selector_from_name},
     },
     macros::felt_hex,
-    providers::{JsonRpcClient, jsonrpc::HttpTransport},
 };
 use starknet_crypto::Felt;
 use url::Url;
 
-use pragma_common::{Pair, starknet::StarknetNetwork};
+use pragma_common::{
+    Pair,
+    starknet::{StarknetNetwork, fallback_provider::FallbackProvider},
+};
 use pragma_entities::InfraError;
 
 pub const ENV_MAINNET_RPC_URL: &str = "MAINNET_RPC_URL";
 pub const ENV_SEPOLIA_RPC_URL: &str = "SEPOLIA_RPC_URL";
 
-pub type RpcClients = HashMap<StarknetNetwork, Arc<JsonRpcClient<HttpTransport>>>;
+pub type RpcClients = HashMap<StarknetNetwork, FallbackProvider>;
 
 pub static ORACLE_ADDRESS_PER_NETWORK: LazyLock<HashMap<StarknetNetwork, Felt>> =
     LazyLock::new(|| {
@@ -37,31 +36,59 @@ pub static ORACLE_ADDRESS_PER_NETWORK: LazyLock<HashMap<StarknetNetwork, Felt>> 
         addresses
     });
 
+/// The list of all the starknet rpcs that the FallbackProvider may use.
+/// They're sorted by priority (so we sorted them by reliability here).
+pub const MAINNET_STARKNET_RPC_URLS: [&str; 10] = [
+    "https://api.cartridge.gg/x/starknet/mainnet",
+    "https://starknet-mainnet.g.alchemy.com/starknet/version/rpc/v0_8/WrkE4HqPXT-zi7gQn8bUtH-TXgYYs3w1",
+    "https://starknet-mainnet.blastapi.io/d4c81751-861c-4970-bef5-9decd7f7aa39",
+    "https://starknet-mainnet.infura.io/v3/1e978c4df1984be09e18e5cd849228e4",
+    "https://mainnet-pragma.karnot.xyz/",
+    "https://api.zan.top/public/starknet-mainnet",
+    "https://starknet.api.onfinality.io/public",
+    "https://rpc.starknet.lava.build:443",
+    "https://starknet-mainnet.reddio.com",
+    "https://starknet.drpc.org",
+];
+
+pub const SEPOLIA_STARKNET_RPC_URLS: [&str; 6] = [
+    "https://api.cartridge.gg/x/starknet/sepolia",
+    "https://sepolia-pragma.karnot.xyz/",
+    "https://rpc.starknet-testnet.lava.build:443",
+    "https://starknet-sepolia.reddio.com",
+    "https://starknet-sepolia.drpc.org",
+    "https://starknet-sepolia.public.blastapi.io",
+];
+
 /// Init the RPC clients based on the provided ENV variables.
 /// Panics if the env are not correctly set.
-pub fn init_rpc_clients() -> HashMap<StarknetNetwork, Arc<JsonRpcClient<HttpTransport>>> {
-    let mainnet_rpc_url: Url = std::env::var(ENV_MAINNET_RPC_URL)
-        .unwrap_or("https://free-rpc.nethermind.io/mainnet-juno".to_string())
-        .parse()
-        .expect("Invalid MAINNET_RPC_URL provided.");
-    let mainnet_client = JsonRpcClient::new(HttpTransport::new(mainnet_rpc_url));
+pub fn init_rpc_clients() -> HashMap<StarknetNetwork, FallbackProvider> {
+    let mainnet_starknet_provider = FallbackProvider::new(
+        MAINNET_STARKNET_RPC_URLS
+            .iter()
+            .map(|url| Url::parse(url).unwrap())
+            .collect(),
+    )
+    .expect("Could not init the starknet provider");
 
-    let sepolia_rpc_url: Url = std::env::var(ENV_SEPOLIA_RPC_URL)
-        .unwrap_or("https://free-rpc.nethermind.io/sepolia-juno".to_string())
-        .parse()
-        .expect("Invalid SEPOLIA_RPC_URL provided.");
-    let sepolia_client = JsonRpcClient::new(HttpTransport::new(sepolia_rpc_url));
+    let sepolia_starknet_provider = FallbackProvider::new(
+        SEPOLIA_STARKNET_RPC_URLS
+            .iter()
+            .map(|url| Url::parse(url).unwrap())
+            .collect(),
+    )
+    .expect("Could not init the starknet provider");
 
     let mut rpc_clients = HashMap::new();
-    rpc_clients.insert(StarknetNetwork::Mainnet, Arc::new(mainnet_client));
-    rpc_clients.insert(StarknetNetwork::Sepolia, Arc::new(sepolia_client));
+    rpc_clients.insert(StarknetNetwork::Mainnet, mainnet_starknet_provider);
+    rpc_clients.insert(StarknetNetwork::Sepolia, sepolia_starknet_provider);
 
     rpc_clients
 }
 
 /// Calls the `get_decimals` endpoint of pragma oracle and returns the result.
 pub async fn call_get_decimals(
-    rpc_client: &Arc<JsonRpcClient<HttpTransport>>,
+    rpc_client: &FallbackProvider,
     pair: &Pair,
     network: StarknetNetwork,
 ) -> Result<u32, InfraError> {
